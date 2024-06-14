e2777dbd6228dbb15b25d6ee2a9c2ecb95efc8c0
Aristo <darkvale57@gmail.com>
2024-06-14 15:48:32 -0400

    added more games/untrained
    games added:
    Subnautica
    Doom Eternal
    Rainbow Six Siege
    
    Additional notes:
    Can use this iteration model, more optimized for all games besides those just added.

---
+++ games to add.py
@@ -0,0 +1,24 @@
+persona series
+gmod
+jackbox
+subnautica
+street fighter 6
+hell divers?
+elden ring
+red dead redemption
+hogwarts legacy
+hades 2
+sea of thieves
+smash
+destiny 2
+gtaV
+dead by daylight
+elden ring
+the sims 4
+the sims 3
+rust
+sea of thieves
+stardew valley
+cuphead
+palworld
+overcooked
